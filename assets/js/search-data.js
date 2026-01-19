9a8bc43fd21f78f434df9b512e0ae0afa17d00a4
mdhdtouki <142306566+mdhdtouki@users.noreply.github.com>
2026-01-19 15:44:03 +0000

    Deploying to gh-pages from @ mdhdtouki/mdhdtouki.github.io@514a643bc66c1c2fe09945aa2e931d339dd741fb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -521,7 +521,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%68.%74%6F%75%6B%69@%67%77%75.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
